dd92ac5a0b462ee7398c818d1afec67884409920
Krishna Avtar <113005173+krsnavtr-code@users.noreply.github.com>
2024-02-28 20:09:37 +0530

    modify the formatTime function

--- src/Component/Stopwatch.jsx
+++ src/Component/Stopwatch.jsx
@@ -30,9 +30,7 @@ const Stopwatch = () => {
   const formatTime = () => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
-    return `${minutes < 10 ? "0" : ""}${minutes}:${
-      seconds < 10 ? "0" : ""
-    }${seconds}`;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
 
   return (
